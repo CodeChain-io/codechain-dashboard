d549f4986b7364d0173905770a8a28acdcdb409a
Gilyoung Kim <roadzero@kodebox.io>
2018-09-11 12:14:50 +0900

    Move index.scss file to the styles folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
+import "./styles/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
