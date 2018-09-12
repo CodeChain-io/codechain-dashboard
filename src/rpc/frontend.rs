0fde7bcbac05ae3bd30013fccfb2fabd327f3406
Juhyung Park <jh@kodebox.io>
2018-09-12 18:28:25 +0900

    Add dummy version of node_getInfo

--- src/rpc/frontend.rs
+++ src/rpc/frontend.rs
@@ -1,5 +1,8 @@
 use super::router::Router;
-use super::types::{BlockId, DashboardGetNetworkResponse, DashboardNode};
+use super::types::{
+    BlockId, DashboardGetNetworkResponse, DashboardNode, HardwareInfo, HardwareUsage, NetworkPermission,
+    NodeGetInfoResponse,
+};
 
 pub fn add_routing(routing_table: &mut Router) {
     let f: fn() -> String = ping;
@@ -7,7 +10,8 @@ pub fn add_routing(routing_table: &mut Router) {
     routing_table.add_route("ping", Box::new(ping as fn() -> String));
     routing_table.add_route("add1", Box::new(add1 as fn(i32) -> i32));
     routing_table
-        .add_route("dashboard_getNetwork", Box::new(dashboard_get_network as fn() -> DashboardGetNetworkResponse))
+        .add_route("dashboard_getNetwork", Box::new(dashboard_get_network as fn() -> DashboardGetNetworkResponse));
+    routing_table.add_route("node_getInfo", Box::new(node_get_info as fn() -> NodeGetInfoResponse));
 }
 
 fn ping() -> String {
@@ -30,5 +34,40 @@ fn dashboard_get_network() -> DashboardGetNetworkResponse {
             pending_parcel_count: 0,
         }],
         connections: Vec::new(),
+    }
+}
+
+fn node_get_info() -> NodeGetInfoResponse {
+    NodeGetInfoResponse {
+        version: "0.1.0".to_string(),
+        commit_hash: "84e70586dea8e6b4021d65b8164bbac28cb88ecb".to_string(),
+        best_block_id: BlockId {
+            number: 0,
+            hash: Default::default(),
+        },
+        pending_parcels: Vec::new(),
+        peers: Vec::new(),
+        whitelist: NetworkPermission {
+            list: Vec::new(),
+            enabled: false,
+        },
+        blacklist: NetworkPermission {
+            list: Vec::new(),
+            enabled: false,
+        },
+        hardware: HardwareInfo {
+            cpu_usage: 3.4,
+            disk_usage: HardwareUsage {
+                total: "3GB".to_string(),
+                available: "5GB".to_string(),
+                percentage_used: "60%".to_string(),
+            },
+            memory_usage: HardwareUsage {
+                total: "3GB".to_string(),
+                available: "5GB".to_string(),
+                percentage_used: "60%".to_string(),
+            },
+        },
+        events: vec!["Network connected".to_string(), "Block received".to_string()],
     }
 }
--- src/rpc/types.rs
+++ src/rpc/types.rs
@@ -4,11 +4,42 @@ use cprimitives::H256;
 
 pub type NodeVersion = String;
 
+pub type Event = String;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Parcel {
+    // FIXME: fill structure
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NetworkPermission {
+    pub list: Vec<SocketAddr>,
+    pub enabled: bool,
+}
+
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BlockId {
     pub number: i64,
     pub hash: H256,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HardwareUsage {
+    pub total: String,
+    pub available: String,
+    pub percentage_used: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HardwareInfo {
+    pub cpu_usage: f64,
+    pub disk_usage: HardwareUsage,
+    pub memory_usage: HardwareUsage,
 }
 
 #[derive(Debug, Serialize)]
@@ -32,4 +63,18 @@ pub struct NodeConnection {
 pub struct DashboardGetNetworkResponse {
     pub nodes: Vec<DashboardNode>,
     pub connections: Vec<NodeConnection>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NodeGetInfoResponse {
+    pub version: NodeVersion,
+    pub commit_hash: String,
+    pub best_block_id: BlockId,
+    pub pending_parcels: Vec<Parcel>,
+    pub peers: Vec<SocketAddr>,
+    pub whitelist: NetworkPermission,
+    pub blacklist: NetworkPermission,
+    pub hardware: HardwareInfo,
+    pub events: Vec<Event>,
 }
