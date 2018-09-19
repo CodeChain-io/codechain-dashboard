4f4243a2890343431068f1274c1a94cef703d969
Juhyung Park <jh@kodebox.io>
2018-09-19 18:38:47 +0900

    Serve log file using HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate log;
+extern crate iron;
 
 extern crate codechain_rpc as crpc;
 extern crate jsonrpc_core;
@@ -21,14 +22,19 @@ mod router;
 mod rpc;
 
 use std::cell::Cell;
+use std::fmt;
 use std::rc::Rc;
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
+use iron::prelude::*;
+use iron::status;
 use ws::listen;
 
+use self::agent::agent::SendAgentRPC;
 use self::agent::handler::WebSocketHandler as AgentHandler;
-use self::agent::service::Service as AgentService;
+use self::agent::service::{Service as AgentService, ServiceSender as AgentServiceSender};
 use self::frontend::api::add_routing as add_frontend_routing;
 use self::frontend::handler::WebSocketHandler as FrontendHandler;
 use self::frontend::types::Context as FrontendContext;
@@ -40,6 +46,7 @@ fn main() {
 
     let agent_service_sender = AgentService::run_thread();
     let agent_service_for_frontend = agent_service_sender.clone();
+    let web_handler = WebHandler::new(agent_service_sender.clone());
 
     let frontend_join = thread::Builder::new()
         .name("frontend listen".to_string())
@@ -68,6 +75,83 @@ fn main() {
         })
         .expect("Should success listening agent");
 
+    let webserver_join = thread::Builder::new()
+        .name("webserver".to_string())
+        .spawn(move || {
+            let _server = Iron::new(web_handler).http("localhost:5012").unwrap();
+            cinfo!("Webserver listening on 5012");
+        })
+        .expect("Should success open webserver");
+
     frontend_join.join().expect("Join frotend listner");
     agent_join.join().expect("Join agent listner");
+    webserver_join.join().expect("Join webserver");
 }
+
+struct WebHandler {
+    agent_service_sender: Mutex<AgentServiceSender>,
+}
+
+impl WebHandler {
+    fn new(agent_service_sender: AgentServiceSender) -> Self {
+        Self {
+            agent_service_sender: Mutex::new(agent_service_sender),
+        }
+    }
+}
+
+impl iron::Handler for WebHandler {
+    fn handle(&self, req: &mut iron::Request) -> IronResult<iron::Response> {
+        let paths = req.url.path();
+        if paths.len() != 2 {
+            cwarn!("Invalid web request {}", req.url);
+            return Ok(Response::with(status::NotFound))
+        }
+
+        if paths.get(0).expect("Already checked") != &"log" {
+            cwarn!("Invalid web request {}", req.url);
+            return Ok(Response::with(status::NotFound))
+        }
+
+        let node_address = paths.get(1).expect("Already checked");
+        let node_address = node_address
+            .parse()
+            .map_err(|_| iron::IronError::new(WebError::new("Invalid socket address"), status::BadRequest))?;
+        ctrace!("Get log for agent-{}", node_address);
+
+        let agent = self
+            .agent_service_sender
+            .lock()
+            .expect("Should success get lock")
+            .get_agent(node_address)
+            .ok_or_else(|| iron::IronError::new(WebError::new("Not Found"), status::NotFound))?;
+
+        let log =
+            agent.shell_get_codechain_log().map_err(|err| iron::IronError::new(err, status::InternalServerError))?;
+
+        use iron::mime;
+        let content_type = "text/plain".parse::<mime::Mime>().unwrap();
+        Ok(Response::with((content_type, status::Ok, log)))
+    }
+}
+
+#[derive(Debug)]
+struct WebError {
+    value: String,
+}
+
+impl WebError {
+    fn new(s: &str) -> Self {
+        WebError {
+            value: s.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for WebError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+impl std::error::Error for WebError {}
