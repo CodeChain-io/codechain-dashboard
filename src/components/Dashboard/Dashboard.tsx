2f8aabb5c66bb8d72d3744425c5a9a5bca00541e
Gilyoung Kim <roadzero@kodebox.io>
2018-09-21 15:16:07 +0900

    Add a status panel to the dashboard page

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,3 +1,6 @@
+import { faCircle, faInfo } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as _ from "lodash";
 import * as React from "react";
 import { connect, DispatchProp } from "react-redux";
 import { Dispatch } from "redux";
@@ -6,6 +9,7 @@ import { RootState } from "../../reducers";
 import { Apis } from "../../requests";
 import { ChainNetworks } from "../../requests/types";
 import { ConnectionGraphContainer } from "./ConnectGraphContainer/ConnectionGraphContainer";
+import "./Dashboard.css";
 
 interface OwnProps {
   chainNetworks: ChainNetworks | undefined;
@@ -24,11 +28,112 @@ class Dashboard extends React.Component<Props> {
       return <div>Loading...</div>;
     }
     return (
-      <div className="dashboard vh-100">
-        <ConnectionGraphContainer
-          chainNetworks={chainNetworks}
-          className="animated fadeIn"
-        />
+      <div className="dashboard d-flex">
+        <div className="left-panel mr-3">
+          <ConnectionGraphContainer
+            chainNetworks={chainNetworks}
+            className="animated fadeIn"
+          />
+        </div>
+        <div className="right-panel">
+          <div className="connection-graph-help animated fadeIn">
+            <div className="connection-graph-help-header">
+              <h5 className="mb-0">
+                <FontAwesomeIcon className="mr-2" icon={faInfo} />
+                Status
+              </h5>
+            </div>
+            <div className="connection-graph-help-body">
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <div className="d-flex align-items-center mb-1">
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      className="text-success mr-2"
+                    />
+                    <span className="mr-auto">Run</span>
+                    <span>
+                      {
+                        _.filter(
+                          chainNetworks.nodes,
+                          node => node.status === "Run"
+                        ).length
+                      }
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="d-flex align-items-center mb-1">
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      className="text-warning mr-2"
+                    />
+                    <span className="mr-auto">Starting</span>
+                    <span>
+                      {
+                        _.filter(
+                          chainNetworks.nodes,
+                          node => node.status === "Starting"
+                        ).length
+                      }
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="d-flex align-items-center mb-1">
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      className="text-secondary mr-2"
+                    />
+                    <span className="mr-auto">Stop</span>
+                    <span>
+                      {
+                        _.filter(
+                          chainNetworks.nodes,
+                          node => node.status === "Stop"
+                        ).length
+                      }
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="d-flex align-items-center mb-1">
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      className="text-danger mr-2"
+                    />
+                    <span className="mr-auto">Error</span>
+                    <span>
+                      {
+                        _.filter(
+                          chainNetworks.nodes,
+                          node => node.status === "Error"
+                        ).length
+                      }
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="d-flex align-items-center mb-1">
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      className="text-info mr-2"
+                    />
+                    <span className="mr-auto">UFO</span>
+                    <span>
+                      {
+                        _.filter(
+                          chainNetworks.nodes,
+                          node => node.status === "UFO"
+                        ).length
+                      }
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
