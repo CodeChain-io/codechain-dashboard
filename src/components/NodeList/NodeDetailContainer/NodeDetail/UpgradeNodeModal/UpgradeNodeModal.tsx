45b2d8e8775d5a050a51de6d96b167d1d2b47acf
Gilyoung Kim <roadzero@kodebox.io>
2018-09-27 15:31:57 +0900

    Modify ui of upgrading node

--- src/components/NodeList/NodeDetailContainer/NodeDetail/UpgradeNodeModal/UpgradeNodeModal.tsx
+++ src/components/NodeList/NodeDetailContainer/NodeDetail/UpgradeNodeModal/UpgradeNodeModal.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as Modal from "react-modal";
+import { Label } from "reactstrap";
 import "./UpgradeNodeModal.css";
 const customStyles = {
   content: {
@@ -18,12 +19,20 @@ interface Props {
   isOpen: boolean;
 }
 
-export default class UpgradeNodeModal extends React.Component<Props> {
+interface State {
+  selectedType: string;
+}
+
+export default class UpgradeNodeModal extends React.Component<Props, State> {
   public constructor(props: Props) {
     super(props);
+    this.state = {
+      selectedType: "upgrade-by-branch"
+    };
   }
   public render() {
     const { isOpen, onClose } = this.props;
+    const { selectedType } = this.state;
     return (
       <div>
         <Modal
@@ -34,17 +43,83 @@ export default class UpgradeNodeModal extends React.Component<Props> {
           contentLabel="Upgrade node popup"
         >
           <div className="upgrade-node-modal animated fadeIn">
-            <div className="d-flex">
-              <div className="branch-container">branch list</div>
-              <div className="commit-container">Commit list</div>
+            <div>
+              <div>
+                <div className="form-check">
+                  <input
+                    type="radio"
+                    className="form-check-input"
+                    id="upgrade-by-branch"
+                    name="upgrade-type"
+                    value="upgrade-by-branch"
+                    checked={selectedType === "upgrade-by-branch"}
+                    // tslint:disable-next-line:jsx-no-lambda
+                    onChange={e =>
+                      this.setState({ selectedType: e.target.value })
+                    }
+                  />
+                  <Label className="form-check-label" for="upgrade-by-branch">
+                    Upgrade by branch
+                  </Label>
+                </div>
+                {selectedType === "upgrade-by-branch" && (
+                  <div className="mt-3 mb-3">
+                    <select id="inputState" className="form-control">
+                      <option selected={true}>Choose...</option>
+                      <option>...</option>
+                    </select>
+                  </div>
+                )}
+              </div>
+              <div className="form-check">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  id="upgrade-by-tag"
+                  name="upgrade-type"
+                  value="upgrade-by-tag"
+                  checked={selectedType === "upgrade-by-tag"}
+                  // tslint:disable-next-line:jsx-no-lambda
+                  onChange={e =>
+                    this.setState({ selectedType: e.target.value })
+                  }
+                />
+                <Label className="form-check-label" for="upgrade-by-tag">
+                  Upgrade by tag
+                </Label>
+              </div>
+              <div className="form-check">
+                <input
+                  type="radio"
+                  className="form-check-input"
+                  id="upgrade-by-commit"
+                  name="upgrade-type"
+                  value="upgrade-by-commit"
+                  checked={selectedType === "upgrade-by-commit"}
+                  // tslint:disable-next-line:jsx-no-lambda
+                  onChange={e =>
+                    this.setState({ selectedType: e.target.value })
+                  }
+                />
+                <Label className="form-check-label" for="upgrade-by-commit">
+                  Upgrade by commit hash
+                </Label>
+              </div>
             </div>
             <div className="d-flex justify-content-end">
               <button
                 type="submit"
                 onClick={this.onCloseClick}
-                className="btn btn-secondary mt-3"
+                className="btn btn-secondary mt-3 mr-3"
               >
                 Cancel
+              </button>
+              <button
+                type="submit"
+                onClick={this.onCloseClick}
+                className="btn btn-primary mt-3"
+              >
+                Upgrade
               </button>
             </div>
           </div>
