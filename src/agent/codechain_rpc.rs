a19c3d0c53db6f75ac10f9d2b27ce66b0b31fdf9
Juhyung Park <jh@kodebox.io>
2018-10-01 15:29:16 +0900

    Refactor codechain_rpc functions

--- src/agent/codechain_rpc.rs
+++ src/agent/codechain_rpc.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 
 use jsonrpc_core::types::{Failure, Output, Success};
+use serde::de::DeserializeOwned;
 use serde_json;
 
 use super::super::common_rpc_types::{BlockId, NodeStatus};
@@ -19,40 +20,29 @@ impl CodeChainRPC {
     }
 
     pub fn get_peers(&self, status: NodeStatus) -> Result<Vec<SocketAddr>, String> {
-        if status != NodeStatus::Run {
-            return Ok(Vec::new())
-        }
-
-        let response = self
-            .sender
-            .codechain_call_rpc(("net_getEstablishedPeers".to_string(), Vec::new()))
-            .map_err(|err| format!("{}", err))?;
-
-        let peers: Vec<SocketAddr> = match response {
-            Output::Success(Success {
-                result,
-                ..
-            }) => serde_json::from_value(result).map_err(|err| format!("{}", err))?,
-            Output::Failure(Failure {
-                error,
-                ..
-            }) => return Err(format!("get_peers error {:#?}", error)),
-        };
-
-        Ok(peers)
+        self.call_rpc(status, "net_getEstablishedPeers")
     }
 
     pub fn get_best_block_id(&self, status: NodeStatus) -> Result<Option<BlockId>, String> {
+        let response: Option<ChainGetBestBlockIdResponse> = self.call_rpc(status, "chain_getBestBlockId")?;
+
+        Ok(response.map(|response| BlockId {
+            block_number: response.number,
+            hash: response.hash,
+        }))
+    }
+
+    fn call_rpc<T>(&self, status: NodeStatus, method: &str) -> Result<T, String>
+    where
+        T: Default + DeserializeOwned, {
         if status != NodeStatus::Run {
-            return Ok(None)
+            return Ok(Default::default())
         }
 
-        let response = self
-            .sender
-            .codechain_call_rpc(("chain_getBestBlockId".to_string(), Vec::new()))
-            .map_err(|err| format!("{}", err))?;
+        let response =
+            self.sender.codechain_call_rpc((method.to_string(), Vec::new())).map_err(|err| format!("{}", err))?;
 
-        let best_block_id: ChainGetBestBlockIdResponse = match response {
+        let response: T = match response {
             Output::Success(Success {
                 result,
                 ..
@@ -63,9 +53,6 @@ impl CodeChainRPC {
             }) => return Err(format!("get_peers error {:#?}", error)),
         };
 
-        Ok(Some(BlockId {
-            block_number: best_block_id.number,
-            hash: best_block_id.hash,
-        }))
+        Ok(response)
     }
 }
