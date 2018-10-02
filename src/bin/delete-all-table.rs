80539cf8d3dae92a2ba9925488823bdf07b7c53f
Juhyung Park <jh@kodebox.io>
2018-10-02 17:14:28 +0900

    Add delete-all-table and generate-schema command

---
+++ src/bin/delete-all-table.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate codechain_agent_hub as chub;
+extern crate postgres;
+#[macro_use]
+extern crate log;
+
+use chub::logger_init;
+use postgres::{Connection, TlsMode};
+
+fn main() {
+    logger_init().expect("Logger should be initialized");
+
+    // FIXME: move to configuration file
+    let user = "codechain-agent-hub";
+    let password = "preempt-entreat-bell-chanson";
+    let conn_uri = format!("postgres://{}:{}@localhost", user, password);
+    let conn = Connection::connect(conn_uri, TlsMode::None).unwrap();
+
+    let table_names = get_all_table_names(&conn);
+    cinfo!("Table names to delete are {:?}", table_names);
+
+    for table_name in table_names {
+        cinfo!("Drop table {}", &table_name);
+        drop_table(&conn, &table_name);
+    }
+}
+
+fn get_all_table_names(conn: &Connection) -> Vec<String> {
+    let rows =
+        conn.query(
+            "SELECT table_name \
+             FROM information_schema.tables \
+             WHERE table_schema='public' \
+             AND table_type='BASE TABLE'",
+            &[],
+        ).unwrap();
+    rows.iter().map(|row| row.get(0)).collect()
+}
+
+fn drop_table(conn: &Connection, table_name: &str) {
+    conn.execute(&format!("DROP TABLE {}", table_name), &[]).unwrap();
+}
--- src/bin/generate-schema.rs
+++ src/bin/generate-schema.rs
@@ -1,15 +1,32 @@
+#[macro_use]
+extern crate codechain_agent_hub as chub;
 extern crate postgres;
+#[macro_use]
+extern crate log;
 
+use chub::logger_init;
 use postgres::{Connection, TlsMode};
 
 fn main() {
+    logger_init().expect("Logger should be initialized");
+
     // FIXME: move to configuration file
     let user = "codechain-agent-hub";
     let password = "preempt-entreat-bell-chanson";
     let conn_uri = format!("postgres://{}:{}@localhost", user, password);
     let conn = Connection::connect(conn_uri, TlsMode::None).unwrap();
-    let result = conn.query("SELECT 1", &[]).unwrap();
 
-    println!("Query result is {:?}", result);
-    println!("Hi I'm generate-schema");
+    create_agent_extra_schema(&conn);
+}
+
+fn create_agent_extra_schema(conn: &Connection) {
+    cinfo!("Create agent_extra table");
+    conn.execute(
+        "CREATE TABLE agent_extra (\
+        id SERIAL PRIMARY KEY,
+        prev_env VARCHAR NOT NULL,
+        prev_args VARCHAR NOT NULL
+    )",
+        &[],
+    ).unwrap();
 }
