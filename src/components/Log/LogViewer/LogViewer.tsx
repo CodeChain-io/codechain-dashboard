db245f7f25ad37f073c0cbe6675ef87c7e488971
Gilyoung <roadzero@kodebox.io>
2018-11-07 12:05:45 +0900

    Chagne text color according to brightness of the background color on the log page

--- src/components/Log/LogViewer/LogViewer.tsx
+++ src/components/Log/LogViewer/LogViewer.tsx
@@ -62,10 +62,14 @@ class LogViewer extends React.Component<Props, any> {
                   style={
                     nodeColors[log.nodeName]
                       ? {
-                          backgroundColor: nodeColors[log.nodeName]
+                          backgroundColor: nodeColors[log.nodeName],
+                          color: this.getColorByBackground(
+                            nodeColors[log.nodeName]
+                          )
                         }
                       : {
-                          backgroundColor: "#ffffff"
+                          backgroundColor: "#ffffff",
+                          color: "#000000"
                         }
                   }
                 >
@@ -97,6 +101,30 @@ class LogViewer extends React.Component<Props, any> {
   private toggleOrder = () => {
     this.props.dispatch(
       changeOrder(this.props.orderBy === "DESC" ? "ASC" : "DESC")
+    );
+  };
+  private getColorByBackground = (hexBackground: string) => {
+    const brightness = this.getBrightness(this.hexToRgb(hexBackground));
+    return brightness > 0.5 ? "#000000" : "#ffffff";
+  };
+  private hexToRgb = (hex: string) => {
+    return hex
+      .replace(
+        /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+        (m, r, g, b) => "#" + r + r + g + g + b + b
+      )
+      .substring(1)
+      .match(/.{2}/g)!
+      .map(x => parseInt(x, 16));
+  };
+  private getBrightness = (rgb: number[]) => {
+    const R = rgb[0] / 255;
+    const G = rgb[1] / 255;
+    const B = rgb[2] / 255;
+    return Math.sqrt(
+      0.299 * Math.pow(R, 2.2) +
+        0.587 * Math.pow(G, 2.2) +
+        0.114 * Math.pow(B, 2.2)
     );
   };
 }
