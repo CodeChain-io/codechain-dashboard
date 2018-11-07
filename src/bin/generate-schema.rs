2dde5bbf170e854b1605ca61eddb02deb6f5b438
Juhyung Park <jh@kodebox.io>
2018-11-07 16:39:25 +0900

    Add logs schema

--- src/bin/generate-schema.rs
+++ src/bin/generate-schema.rs
@@ -17,12 +17,13 @@ fn main() {
     let conn = Connection::connect(conn_uri, TlsMode::None).unwrap();
 
     create_agent_extra_schema(&conn);
+    create_logs_schema(&conn);
 }
 
 fn create_agent_extra_schema(conn: &Connection) {
     cinfo!("Create agent_extra table");
     conn.execute(
-        "CREATE TABLE agent_extra (\
+        "CREATE TABLE IF NOT EXISTS agent_extra (
         id SERIAL PRIMARY KEY,
         name VARCHAR NOT NULL UNIQUE,
         prev_env VARCHAR NOT NULL,
@@ -31,4 +32,22 @@ fn create_agent_extra_schema(conn: &Connection) {
         &[],
     )
     .unwrap();
+}
+
+fn create_logs_schema(conn: &Connection) {
+    cinfo!("Create logs table");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS logs (\
+        id SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        target VARCHAR NOT NULL,
+        message VARCHAR NOT NULL,
+        timestamp TIME WITH TIME ZONE NOT NULL
+    )",
+        &[],
+    )
+    .unwrap();
+
+    cinfo!("Create logs_timestamp index");
+    conn.execute("CREATE INDEX IF NOT EXISTS logs_timestamp ON logs (timestamp)", &[]).unwrap();
 }
