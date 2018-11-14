823317e169d252cd8b83b017144aea702eb79b0c
Juhyung Park <jh@kodebox.io>
2018-11-14 17:05:32 +0900

    Read timestamp with nanosecond

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -12,7 +12,8 @@ use super::super::types::{Log, LogQueryParams};
 pub fn insert(conn: &postgres::Connection, node_name: &NodeName, logs: Vec<StructuredLog>) -> postgres::Result<()> {
     ctrace!("Add log {} : {:?}", node_name, logs);
     for log in logs {
-        let datetime = chrono::DateTime::parse_from_rfc3339(&log.timestamp).unwrap();
+        let rfc3339with_nano_second = "%Y-%m-%dT%H:%M:%S.%f%z";
+        let datetime = chrono::DateTime::parse_from_str(&log.timestamp, rfc3339with_nano_second).unwrap();
         conn.execute(
             "INSERT INTO logs (name, level, target, message, timestamp) VALUES ($1, $2, $3, $4, $5)",
             &[node_name, &log.level, &log.target, &log.message, &datetime],
