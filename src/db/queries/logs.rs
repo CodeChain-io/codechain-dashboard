8eba0aaa5d9bdb79418933d9eed520e09fc13de8
Juhyung Park <jh@kodebox.io>
2019-01-31 11:26:55 +0900

    Remove order by id
    The "order by id" was needed when the timestamp field's unit was a
    second. Now, since the unit is micro, we do not need it any more.

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -99,7 +99,7 @@ pub fn search(conn: &postgres::Connection, params: LogQueryParams) -> postgres::
     };
 
     let order_by = params.order_by.unwrap_or(OrderBy::ASC);
-    let order_by_clause = format!("ORDER BY timestamp {:?}, id {:?}", order_by, order_by);
+    let order_by_clause = format!("ORDER BY timestamp {:?}", order_by);
 
     let limit = params.item_per_page.unwrap_or(100);
     let limit_clause = format!("LIMIT {}", limit);
