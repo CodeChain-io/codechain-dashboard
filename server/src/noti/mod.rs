8908a5b1e08d62a82a884e82228f709a27e9038e
Park Juhyung <jh@kodebox.io>
2019-05-29 14:32:29 +0900

    Add `Noti::info` method

--- server/src/noti/mod.rs
+++ server/src/noti/mod.rs
@@ -41,13 +41,7 @@ pub struct Noti {
 
 impl Noti {
     pub fn warn(&self, network_id: &str, message: &str) {
-        let mut targets = Vec::with_capacity(2);
-        if self.slack.is_some() {
-            targets.push("slack");
-        }
-        if self.sendgrid.is_some() {
-            targets.push("sendgrid");
-        }
+        let targets = self.targets();
         if targets.is_empty() {
             cinfo!("No targets to send warning: {}", message);
             return
@@ -67,5 +61,39 @@ impl Noti {
                 cwarn!("Cannot send an email({}): {}", message, err);
             }
         }
+    }
+
+    pub fn info(&self, network_id: &str, title: &str, message: &str) {
+        let targets = self.targets();
+        if targets.is_empty() {
+            cinfo!("No targets to send info: {}", message);
+            return
+        }
+        cinfo!("Send a info to {}: {}", targets.join(", "), message);
+
+        if let Some(slack) = self.slack.as_ref() {
+            if let Err(err) = slack.send(format!("{}: {}", network_id, message)) {
+                cwarn!("Cannot send a slack message({}): {}", message, err);
+            }
+        }
+        if let Some(sendgrid) = self.sendgrid.as_ref() {
+            if let Err(err) = sendgrid.send(
+                format!("[info][{}][dashboard-server] {} at {}", network_id, title, Utc::now().to_rfc3339()),
+                message,
+            ) {
+                cwarn!("Cannot send an email({}): {}", message, err);
+            }
+        }
+    }
+
+    fn targets(&self) -> Vec<&str> {
+        let mut targets = Vec::with_capacity(2);
+        if self.slack.is_some() {
+            targets.push("slack");
+        }
+        if self.sendgrid.is_some() {
+            targets.push("sendgrid");
+        }
+        targets
     }
 }
