3ac743bf33c83f49c6b8008b58a0978af364c7d5
Park Juhyung <jh@kodebox.io>
2019-05-30 16:17:39 +0900

    Show database connection status in the daily report

--- server/src/daily_reporter.rs
+++ server/src/daily_reporter.rs
@@ -3,9 +3,10 @@ use std::thread;
 
 use chrono;
 
+use super::db::ServiceSender as DBServiceSender;
 use super::noti::Noti;
 
-pub fn start(noti: Arc<Noti>) -> thread::JoinHandle<()> {
+pub fn start(noti: Arc<Noti>, db_service: DBServiceSender) -> thread::JoinHandle<()> {
     let network_id = std::env::var("NETWORK_ID").unwrap();
 
     thread::Builder::new()
@@ -16,7 +17,7 @@ pub fn start(noti: Arc<Noti>) -> thread::JoinHandle<()> {
             loop {
                 let new_date = chrono::Utc::now().date();
                 if new_date != current_date {
-                    send_daily_report(&network_id, Arc::clone(&noti));
+                    send_daily_report(&network_id, Arc::clone(&noti), db_service.clone());
                 }
                 current_date = new_date;
                 thread::sleep(std::time::Duration::from_secs(1000));
@@ -25,6 +26,12 @@ pub fn start(noti: Arc<Noti>) -> thread::JoinHandle<()> {
         .unwrap()
 }
 
-pub fn send_daily_report(network_id: &str, noti: Arc<Noti>) {
-    noti.info(network_id, "Daily report", "CodeChain Server is running")
+pub fn send_daily_report(network_id: &str, noti: Arc<Noti>, db_service: DBServiceSender) {
+    let result = db_service.check_connection();
+    let db_status = match result {
+        Ok(_) => "DB is connected".to_string(),
+        Err(err) => format!("DB connection has an error : {:?}", err),
+    };
+    let messages = ["CodeChain Server is running".to_string(), db_status];
+    noti.info(network_id, "Daily report", &messages.join("\n"))
 }
